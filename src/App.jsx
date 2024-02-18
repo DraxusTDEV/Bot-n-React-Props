715ecb1e3b420cddbf59ff9f3482178023c5b885
DraxusTDEV <draxustienda@gmail.com>
2024-02-17 21:03:15 -0500

    Dos botones creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import Button from "./components/Button";
 
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button></Button>
+      <Button texto={"Nuevo Botón"} color={"red"}></Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+const Button = ({ texto = "Text Default", color = "black" }) => {
+  const styles = {
+    width: "100px",
+    background: color,
+    color: "white",
+    height: "30px",
+    borderRadius: "10px",
+    border: "none",
+    cursor: "pointer",
+    margin: "5px",
+  };
+
+  return <button style={styles}>{texto}</button>;
+};
+
+export default Button;
